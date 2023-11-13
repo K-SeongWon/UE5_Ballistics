4845dad2e4a57796a6158d434f9b008432417e71
CastleWon <kimsw9392@gmail.com>
2023-11-13 16:48:13 +0900

    First Commit

---
+++ Source/UE5_Ballistics/UE5_BallisticsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_BallisticsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUE5_BallisticsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUE5_BallisticsGameMode();
+};
+
+
+
